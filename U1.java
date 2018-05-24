a58ddffce2a878a97d78f79d46bf2576101173fc
Martinszyd <martinszyd@poczta.fm>
2018-05-24 11:11:38 +0200

    check at work

--- U1.java
+++ U1.java
@@ -1,6 +1,11 @@
 public class U1 extends Rocket {
 
-    cost = 100000000.0;
+
+    double cost = 100000000.0;
+    double weight = 10.0;
+    double maxWeight = 18.0;
+    double CoLE = (4/100)*(Item.getWeight()/8);
+    double CoLC = (8/100)*(Item.getWeight()/8);
 
 
 }
