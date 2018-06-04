5988fa94de1355ac8182f595b02e9cdc965a9ea1
Martinszyd <martinszyd@poczta.fm>
2018-06-04 08:44:43 +0200

    small corrrection in Item

--- Item.java
+++ Item.java
@@ -11,20 +11,4 @@ public class Item {
         this.weight = weight;
     }
 
-
-    public String getName() {
-        return name;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
 }
